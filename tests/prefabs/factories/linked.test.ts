509439462f27b60c26d0847dbd70214f31b689cf
michel <michel.dolstra@bettyblocks.com>
2022-07-07 09:23:10 +0200

    fix: add test for linked option factory

---
+++ tests/prefabs/factories/linked.test.ts
@@ -0,0 +1,22 @@
+import test from 'tape';
+import { linked } from '../../../src/prefabs/factories/options';
+
+test('linked builds a linked option with a value ref', (t) => {
+  const result = linked('my linked label', {
+    value: { ref: {componentId: '#1', optionId: '#2'}},
+  })('0');
+
+  const expected = {
+    value: {
+      ref: {
+        componentId: '#1',
+        optionId: '#2',
+      }
+    },
+    key: '0',
+    type: 'LINKED_OPTION',
+    label: 'my linked label',
+  };
+  t.deepEqual(result, expected);
+  t.end();
+});
