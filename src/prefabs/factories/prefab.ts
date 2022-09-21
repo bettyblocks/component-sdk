449d182993fa5f7dda5fd9e54cbf8b9ace3f416a
Stefan van Delft <stefan.van.delft@bettyblocks.com>
2022-09-21 11:56:45 +0200

    feat(PAGE-2238): Add permission parameter to prepareAction function.

--- src/prefabs/factories/prefab.ts
+++ src/prefabs/factories/prefab.ts
@@ -40,6 +40,7 @@ export type BeforeCreateArgs = {
       properties: Property[],
       actionTemplate: 'create' | 'update' | 'delete' | 'login' | 'empty',
       authenticationProfile?: AuthenticationProfile,
+      permissions?: 'public' | 'private' | 'inherit',
     ) => Promise<PreparedAction>;
     makeBettyInput: (
       prefabName: string,
