3ea3674625f3715a38ab5851add267d06fef48be
michel <michel.dolstra@bettyblocks.com>
2025-01-27 13:32:11 +0100

    feat: extend allowedActions for optionEvents.

--- src/prefabs/factories/optionSetters.ts
+++ src/prefabs/factories/optionSetters.ts
@@ -1,23 +1,29 @@
 type Format = 'propertyLabel' | 'propertyValue' | 'static';
 
-type SetVariableOptionProps = { target: string; format: Format };
-
-type SetModelOptionProps = {
+interface SetBaseOptionProps {
   target: string;
-};
+}
 
-type SetPropertyOptionProps = {
-  target: string;
-};
+interface SetVariableOptionProps extends SetBaseOptionProps {
+  format: Format;
+}
 
 export function setVariableOption({ target, format }: SetVariableOptionProps) {
   return { action: 'setVariable', format, target };
 }
 
-export function setModelOption({ target }: SetModelOptionProps) {
+export function setModelOption({ target }: SetBaseOptionProps) {
   return { action: 'setModel', target };
 }
 
-export function setPropertyOption({ target }: SetPropertyOptionProps) {
+export function setOptionToDefaultValue({ target }: SetBaseOptionProps) {
+  return { action: 'setToDefault', target };
+}
+
+export function setActionJSInputVariableOption({ target }: SetBaseOptionProps) {
+  return { action: 'setActionJSInputVariable', target };
+}
+
+export function setPropertyOption({ target }: SetBaseOptionProps) {
   return { action: 'setProperty', target };
 }
